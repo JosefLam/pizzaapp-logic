2d34a5b62d63b8aeb9f9368240802d096f37ad4c
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2022-10-30 11:22:38 +0000

    stilll dont work

---
+++ ConsoleApp2/Pizza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class pizza
+{
+    public string pizzaname;
+    public bool tomato;
+    public bool cheese;
+    public bool pepparoni;
+
+    public void showtoppings()
+    {
+        Console.WriteLine($"you have ordered: {pizzaname}");
+        Console.WriteLine("This has:");
+        if (tomato)
+        {
+            Console.WriteLine("tomato");
+        }
+        if (cheese)
+        {
+            Console.WriteLine("cheese");
+        }
+        if (pepparoni)
+        {
+            Console.WriteLine("pepparoni");
+        }
+
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -13,36 +13,14 @@ namespace ConsoleApp2
             
             Console.WriteLine("do you want: 1(magerita) or 2(peperoni)");
             string choice = Console.ReadLine();
-            pizza order = new pizza();
-            if string(parse(choice)) ==
+            if (string(int.parse(choice)) == 1)
+            {
+                new Pizza = new Magerita
+            }
+
             
         }
 
     }
-    class pizza
-    {
-        public string pizzaname;
-        public bool tomato;
-        public bool cheese;
-        public bool pepparoni;
-
-        public void showtoppings()
-        {
-            Console.WriteLine($"you have ordered: {pizzaname}");
-            Console.WriteLine("This has:");
-            if (tomato)
-            {
-                Console.WriteLine("tomato");
-            }
-            if (cheese)
-            {
-                Console.WriteLine("cheese");
-            }
-            if (pepparoni)
-            {
-                Console.WriteLine("pepparoni");
-            }
-
-        }
-    }
+    
 }
